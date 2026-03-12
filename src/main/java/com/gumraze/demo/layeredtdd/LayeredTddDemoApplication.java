862f0f0a2a758d1a9fac77867fcbbc0c447b0f01
Gumraze <154658933+Gumraze-git@users.noreply.github.com>
2026-03-12 14:26:54 +0900

    chore: initial commit

---
+++ src/main/java/com/gumraze/demo/layeredtdd/LayeredTddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gumraze.demo.layeredtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredTddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredTddDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gumraze/demo/layeredtdd/LayeredTddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumraze.demo.layeredtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredTddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
